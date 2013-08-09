a919927ea6ef09959f246c78d11d396f1563c2c3
Paul Ecoffet <ecoffet.paul@gmail.com>
2013-08-09 19:07:49 +0200

    Strict mode

--- data/9gag/contentscript.js
+++ data/9gag/contentscript.js
@@ -1,19 +1,25 @@
-self.port.on("like", function (message) {
+self.port.on("like", function () {
+    "use strict";
+    console.log("called");
     inject("GAG.PostController.upvoteEntry(GAG.ListController.findCurrentlyInFocusEntry());");
 });
 
-self.port.on("dislike", function (message){
+self.port.on("dislike", function (){
+    "use strict";
     inject("GAG.PostController.downvoteEntry(GAG.ListController.findCurrentlyInFocusEntry());");
 });
 
-self.port.on("jumpnext", function (message){
+self.port.on("next", function (){
+    "use strict";
+    console.log("go next");
     inject("if (!GAG.Page.isPostPage()){ GAG.ListController.scrollToNextEntry();}");
 });
 
 
 function inject(script)
 {
-    tag = document.createElement("script");
+    "use strict";
+    var tag = document.createElement("script");
     tag.innerHTML = script;
     document.body.appendChild(tag);
     document.body.removeChild(tag);
